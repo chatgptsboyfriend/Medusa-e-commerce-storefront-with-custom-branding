dc98d21c0c5cb2cefb7fb10acaf8bcedfad6fd0a
Conard <ameurneffati@gmail.com>
2026-01-18 13:01:06 +0100

    Update page.tsx

--- src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -1,13 +1,11 @@
+export const dynamic = "force-dynamic"
+
 import { Metadata } from "next"
 import { notFound } from "next/navigation"
 
-import { getCategoryByHandle, listCategories } from "@lib/data/categories"
-import { listRegions } from "@lib/data/regions"
-import { StoreRegion } from "@medusajs/types"
+import { getCategoryByHandle } from "@lib/data/categories"
 import CategoryTemplate from "@modules/categories/templates"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
-
-export const dynamic = "force-dynamic"
 
 type Props = {
   params: Promise<{ category: string[]; countryCode: string }>
@@ -17,57 +15,34 @@ type Props = {
   }>
 }
 
-export async function generateStaticParams() {
-  const product_categories = await listCategories()
-
-  if (!product_categories) {
-    return []
-  }
-
-  const countryCodes = await listRegions().then((regions: StoreRegion[]) =>
-    regions?.map((r) => r.countries?.map((c) => c.iso_2)).flat()
-  )
-
-  const categoryHandles = product_categories.map(
-    (category: any) => category.handle
-  )
-
-  const staticParams = countryCodes
-    ?.map((countryCode: string | undefined) =>
-      categoryHandles.map((handle: any) => ({
-        countryCode,
-        category: [handle],
-      }))
-    )
-    .flat()
-
-  return staticParams
-}
-
 export async function generateMetadata(props: Props): Promise<Metadata> {
   const params = await props.params
+
   try {
     const productCategory = await getCategoryByHandle(params.category)
 
-    const title = productCategory.name + " | Medusa Store"
-
-    const description = productCategory.description ?? `${title} category.`
+    const title = `${productCategory.name} | Medusa Store`
+    const description =
+      productCategory.description ?? `${productCategory.name} category.`
 
     return {
-      title: `${title} | Medusa Store`,
+      title,
       description,
       alternates: {
-        canonical: `${params.category.join("/")}`,
+        canonical: `/${params.countryCode}/categories/${params.category.join(
+          "/"
+        )}`,
       },
     }
-  } catch (error) {
+  } catch {
     notFound()
   }
 }
 
 export default async function CategoryPage(props: Props) {
-  const searchParams = await props.searchParams
   const params = await props.params
+  const searchParams = await props.searchParams
+
   const { sortBy, page } = searchParams
 
   const productCategory = await getCategoryByHandle(params.category)
